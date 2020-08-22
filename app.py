9fd1f5ba562622661bfbc9f99a7e7b43ac943df1
Rikuta Hiratsuka <rimuru930@gmail.com>
2020-08-22 20:43:04 +0900

    リファクタリング: 計算式をf-stringsの中に移動した｡

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 total_amount = int(input('合計金額を入れてね>'))
 total_number_of_people = int(input('合計人数を入れてね>'))
 
-amount_per_person = total_amount // total_number_of_people
-fraction = total_amount % total_number_of_people
-
-print(f'一人あたりの金額は{amount_per_person}円です｡\n端数は{fraction}円です｡')
+print(f'一人あたりの金額は{total_amount // total_number_of_people}円です｡\n端数は{total_amount % total_number_of_people}円です｡')
