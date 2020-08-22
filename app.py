1ef324fe5f6dd23188fc95242ed23ea768ff5d2e
Rikuta Hiratsuka <rimuru930@gmail.com>
2020-08-22 20:22:43 +0900

    合計金額と人数を入れると､一人あたりの支払金額と端数が表示されるように作った｡
    fraction:端数の意味

---
+++ app.py
@@ -0,0 +1,16 @@
+total_amount = int(input())
+
+total_number_of_people = int(input())
+
+print(total_amount)
+
+print(total_number_of_people)
+
+amount_per_person = total_amount / total_number_of_people
+
+print(amount_per_person)
+
+fraction = total_amount%total_number_of_people
+
+print(fraction)
+
